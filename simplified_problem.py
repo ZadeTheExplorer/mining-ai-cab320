17b4e3b5578cc4bbd48d01c9846c4cbe2f89b30f
BGHProjects <34959126+BGHProjects@users.noreply.github.com>
2021-04-21 20:07:24 +1000

    Updated attempt at DP (2D working slowly, 3D tba)

--- simplified_problem.py
+++ simplified_problem.py
@@ -63,13 +63,23 @@ def actions(underground, state):
 def is_dangerous(state, tolerant):
     state = np.array(state)
 
-    if any(np.greater(np.abs(np.diff(state)),
-                          tolerant)):
-        return True
-    return False
-    #print("This is x_diff = ",x_diff)
+    # print("State ", state)
+    # print("np.diff ", np.diff(state))
+    # print("np.abs ", np.abs(np.diff(state)))
+    # print("np.greater ", np.greater(np.abs(np.diff(state)),
+    #                       tolerant))
+    for value in np.greater(np.abs(np.diff(state)),
+                          tolerant):
+        
+        if (value == True).any():
+            return True
+        else:
+            return False
 
-    #return x_diff.any()
+    # if any(np.greater(np.abs(np.diff(state)),
+    #                       tolerant)):
+    #     return True
+    # return False
 
 
 def payoff(array, state):
@@ -90,74 +100,76 @@ def cartesian_product(arr1, arr2):
     # using the product() method
     return list(product(arr1, repeat=arr2.shape[0]))
 
-def goal_state_3d(underground, tolerance=1):
+def goal_state_3d_with_memo(underground):
 
-    #state_product = product(range(0, underground.shape[1] + 1), repeat=underground.shape[0])
+    # Declare return values
+    best_state = ()
+    best_payoff = 0
+    action_list = []
 
-    # for state in state_product:
-    #     print(state)
+    #print(underground.shape)
+    underg1 = underground.flatten(order="C")
 
-    len_z = underground.shape[0]
-    len_x = underground.shape[1]
-    len_y = underground.shape[2]
+    underground_correct = underg1.reshape(3,4,5)
 
-    state_tuple = []
+    state = np.zeros((underground.shape[1],underground.shape[2]))
 
-    for x in range(len_x):
-        y_tuple = (0,)*len_y
-        state_tuple.append(y_tuple)
-    
-    print(state_tuple)
+    whole_memo = dict()
 
-    #sp = product(range(0, (len_z + 1)), repeat=len_x*len_y)
+    def recursive_search(underg, given_state, action=None, memo=None):
 
-    # for state in state_tuple:
-    #     print(state)
-    #     sp = list(product(range(0, len_z), repeat=len_y))
-    #     sp2 = product(sp,sp)
-    #     sp3 = product(sp2, sp)
-        #print(sp)
-    
-    goal_sum = None
-    best_payoff = None
-    
-    for state in state_tuple:
-        
-        if is_dangerous(state, tolerance):
-            continue
+        nonlocal best_payoff
+        nonlocal best_state
+        nonlocal action_list
+        nonlocal whole_memo
 
-        sum_state = payoff(underground, state)
+        state_payoff = payoff(underg, given_state)
 
-        if best_payoff is None or sum_state > best_payoff:
-            best_payoff = sum_state
-            goal_sum = state
+        #print("Given_state ", given_state)
 
 
-    return goal_sum, best_payoff
+        if state_payoff > best_payoff:
+            best_payoff = state_payoff
+            best_state = tuple(given_state)
 
-def goal_state_2d_with_memo(underground, tolerance, memo=None, finished=False):
+        print("State Payoff ", state_payoff)
+        print("Best Payoff ", best_payoff)
+        print("Best State ", best_state)
+
+        given_state_actions = list(actions(underg, given_state))
+
+        whole_memo[str(given_state.flatten(order="C"))] = (given_state_actions, state_payoff)
+
+        #print(whole_memo)
+
+        if len(given_state_actions) > 0:
+
+            for action in given_state_actions:
+
+                new_state = given_state.copy()
+                new_state[action] += 1
+
+                print("New State ", new_state)
+
+                print("String ", str(new_state.flatten(order="C")))
+
+                if str(new_state) not in whole_memo:
+                    recursive_search(underg, new_state, action, whole_memo)
+                        
+        else:
+            return
+
+    recursive_search(underground_correct, state, whole_memo)
+
 
     '''
-    Problem Variables
-        - goal_sum, best_payoff, is_dangerous(?)
-    Recurrence Relation
-        - If (L,P) is our payoff and column location, our possible moves are:
-            - (L, P+P) if we don't change our column location 
-            - (L-n, P+P) if we go back (relative term) a column location
-            - (L+n, P+P) if we go forward (relative term) a column location
-                - Where n is the distance the neighbour is away from the current location
-    Base Cases
-        - The column location/action isn't dangerous
-        - The column location isn't shorter or longer than the depth of the column
+    Use Patrick's find_action_sequence here with best_state
     '''
+    return best_payoff, action_list, best_state
 
-    # Recursive Implementation
-    #   Generate Actions
-    #   If the payoffs of those actions are bigger, add them
-    #   Generate Actions from those Actions
-    #   Repeat
+def goal_state_2d_with_memo(underground):
 
-    # Declare payoffs
+    # Declare return values
     best_state = ()
     best_payoff = 0
     action_list = []
@@ -165,130 +177,55 @@ def goal_state_2d_with_memo(underground, tolerance, memo=None, finished=False):
     # Set up state for recursive implementation
     state = np.zeros(underground.shape[0])
 
-    def recursive_search(underg, given_state, action=None):
+    whole_memo = dict()
+
+    def recursive_search(underg, given_state, action=None, memo=None):
+
+        '''
+        Step 1 - Calculate the actions and payoff for given state
+        Step 1A - Check if payoff is better than previous
+                - If it is, store payoff as new best payoff, and state as best_state
+        Step 2 - Store those in the memo
+        Step 3 - For every possible action, check if its state is in the memo
+                - If it is, skip it/don't do it
+                - If it is not, call recursive search with new state and memo
+
+        '''
 
         nonlocal best_payoff
         nonlocal best_state
         nonlocal action_list
+        nonlocal whole_memo
 
-        print("Given state = ",given_state)
-        
         state_payoff = payoff(underg, given_state)
 
         if state_payoff > best_payoff:
             best_payoff = state_payoff
-            best_state = given_state
+            best_state = tuple(given_state)
+
+        given_state_actions = list(actions(underg, given_state))
+
+        whole_memo[str(given_state)] = (given_state_actions, state_payoff)
         
-        action_list.append(action)
-        
-        print("Best Payoff ", best_payoff)
-        print("Best State ", best_state)
-        print("Action List ", action_list)
+        if len(given_state_actions) > 0:
 
-        ### Version 1: Just gets the next available action
+            for action in given_state_actions:
 
-        if len(list(actions(underg,given_state))) > 0:
+                new_state = given_state.copy()
+                new_state[action] += 1
 
-            next_action = next(actions(underg,given_state))
-
-            print("Next Actions = ", list(actions(underg,given_state)))
-
-            print("Next Action selected = ", next_action)
-
-            new_state = given_state.copy()
-            new_state[next_action] += 1
-
-            recursive_search(underg, new_state, next_action)
+                if str(new_state) not in whole_memo:
+                    recursive_search(underg, new_state, action, whole_memo)
+                        
         else:
             return
-        
-        ### Version 2: loops through every possible action
-        # if len(list(actions(underg,given_state))) > 0:
-        #     print("Actions Length ", len(list(actions(underg,given_state))))
-        #     #for action in state_actions:
-        #     for action in actions(underg, given_state):
-        #         new_state = given_state.copy()
-        #         new_state[action] += 1
-        #         recursive_search(underg, new_state, action)
-        # else:
-        #     return
-    
-    recursive_search(underground, state)
-    
-    return best_state, best_payoff, action_list
 
+    recursive_search(underground, state, whole_memo)
 
-def goal_state_2d(array, tolerant=1):
-    """
-    Find the max of selected
-    :param array: 2d array representing for the underground
-    :param tolerant:
-    :return: goal state
-    """
-    # cumsum_mine_at_that_index = array.copy()
-    # for i in range(0, array.shape[0]):
-    #     for j in range(1, array.shape[1]):
-    #         cumsum_mine_at_that_index[i, j] += cumsum_mine_at_that_index[i, j - 1]
-
-    # state_product = cartesian_product(range(0, array.shape[1] + 1), array)
-    # print("len", len(state_product))
-    state_product = list(product(range(0, array.shape[1] + 1), repeat=array.shape[0]))
-    
-
-    goal_state = None
-    max_sum = None
-    actions_list = []
-    previous_actions = []
-    previous_sums = []
-    num_actions = 0
-
-    cumsum_mine = np.sum(array, axis=1 if array.ndim == 2 else 2)
-
-    #print(cumsum_mine)
-
-    for state in state_product:
-
-        if is_dangerous(state, tolerant):
-            continue
-
-        state_actions = actions(array, state)
-
-        list_actions = list(state_actions)
-        num_actions += 1
-        print("List Actions = ", list_actions, " Num = ",num_actions)
-
-        # if list_actions not in previous_actions:
-        #     previous_actions.append(list_actions)
-        # else:
-        #     #print("Previous Actions found = ", list_actions)
-        #     continue
-
-        
-        sum_state = payoff(array, state)
-        #print("Max Sum = ",max_sum, " Sum State = ", sum_state)
-
-        if sum_state not in previous_sums:
-            previous_sums.append(sum_state)
-        else:
-            #print("Previous State repeated = ",sum_state)
-            continue
-
-        if max_sum is not None:
-            if sum_state < max_sum:
-                continue
-
-        if max_sum is None or sum_state > max_sum:
-            max_sum = sum_state
-            goal_state = state
-
-
-        # #print(list_actions)
-
-        # if len(list_actions) == 1:
-        #     actions_list.append(list_actions)
-
-
-    return goal_state, max_sum, actions_list
+    '''
+    Use Patrick's find_action_sequence here with best_state
+    '''
+    return best_payoff, action_list, best_state
 
 
 def test_1():
@@ -296,7 +233,7 @@ def test_1():
     array = np.random.uniform(low=-10, high=10, size=(5, 4))
 
     t0 = time.time()
-    poss = goal_state_2d(array, 1)
+    poss = goal_state_2d_with_memo(array)
     t1 = time.time()
     print(f'The solver took {t1 - t0} seconds')
     print(array)
@@ -308,7 +245,7 @@ def test_2():
     array = np.random.uniform(low=-10, high=10, size=(10, 4))
 
     t0 = time.time()
-    poss = goal_state_2d(array, 1)
+    poss = goal_state_2d_with_memo(array)
     t1 = time.time()
     print(f'The solver took {t1 - t0} seconds')
     print(array)
@@ -320,7 +257,7 @@ def test_3():
     array = np.random.uniform(low=-10, high=10, size=(5, 20))
 
     t0 = time.time()
-    poss = goal_state_2d(array, 1)
+    poss = goal_state_2d_with_memo(array)
     t1 = time.time()
     print(f'The solver took {t1 - t0} seconds')
     print(array)
@@ -336,7 +273,7 @@ def test_sanity():
                       [-1.231, 1.558, -0.467, -0.371]])
 
     t0 = time.time()
-    poss = goal_state_2d(array, 1)
+    poss = goal_state_2d_with_memo(array)
     t1 = time.time()
     print(f'The solver took {t1 - t0} seconds')
     print(array)
@@ -344,12 +281,8 @@ def test_sanity():
 
 def test_sanity_3d():
 
-    array = np.array([[[ 0.455,  0.049,  2.38,   0.515],
-                        [ 0.801, -0.09,  -1.815,  0.708],
-                        [-0.857, -0.876, -1.936,  0.316]],
-                        [[ 0.579,  1.311, -1.404, -0.236],
-                        [ 0.072, -1.191, -0.839, -0.227],
-                        [ 0.309,  1.188, -3.055,  0.97 ]],
+    array = np.array([ [ [ 0.455,  0.049,  2.38,   0.515], [ 0.801, -0.09,  -1.815,  0.708], [-0.857, -0.876, -1.936,  0.316] ],
+                        [ [ 0.579,  1.311, -1.404, -0.236], [ 0.072, -1.191, -0.839, -0.227], [ 0.309,  1.188, -3.055,  0.97 ] ],
                         [[-0.54,  -0.061,  1.518, -0.466],
                         [-2.183, -1.083,  0.457,  0.874],
                         [-1.623, -0.16,  -0.535,  1.097]],
@@ -361,7 +294,7 @@ def test_sanity_3d():
                         [ 0.097, -0.546, -1.992, -0.296]]])
 
     t0 = time.time()
-    poss = goal_state_3d(array, 1)
+    poss = goal_state_3d_with_memo(array)
     t1 = time.time()
     print(f'The solver took {t1 - t0} seconds')
     print(array)
@@ -374,8 +307,11 @@ def test_memo_2d():
                       [0.212, 0.088, 0.304, 0.604],
                       [-1.231, 1.558, -0.467, -0.371]])
 
+    #array = np.random.uniform(low=-10, high=10, size=(10, 4))
+    #array = np.random.uniform(low=-10, high=10, size=(5, 20))
+
     t0 = time.time()
-    poss = goal_state_2d_with_memo(array, 1)
+    poss = goal_state_2d_with_memo(array)
     t1 = time.time()
     print(f'The solver took {t1 - t0} seconds')
     print(array)
@@ -385,8 +321,8 @@ def test_memo_2d():
 if __name__ == '__main__':
     # The larger mine (larger x), the crazier the time complexity comes
     #test_sanity()
-    #test_sanity_3d()
+    test_sanity_3d()
     #test_1()
     #test_2()
     #test_3()
-    test_memo_2d()
+    #test_memo_2d()
