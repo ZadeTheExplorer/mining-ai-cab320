aeb5496c097a92ee53d361ff313afe69f3631e62
Keith Hall <keithjhall87@gmail.com>
2021-04-25 15:37:50 +1000

    fixed some var names and comments

--- src/mining.py
+++ src/mining.py
@@ -165,25 +165,29 @@ class Mine(search.Problem):
 
         self.dig_tolerance = dig_tolerance
 
-        self.len_x = underground.shape[0]
-        self.len_z = (underground.shape[1] if underground.ndim == 2
-                      else underground.shape[2])
-        self.len_y = underground.shape[1] if underground.ndim == 3 else None
+        self.is_2D = False if underground.ndim == 3 else True
 
+        # Mine lengths
+        self.len_x = underground.shape[0]
+        self.len_z = (underground.shape[1] if self.is_2D
+                      else underground.shape[2])
+        self.len_y = None if self.is_2D else underground.shape[1]
+
+        # The sum of the mine heading down the Z axis (digging down)
         self.cumsum_mine = np.cumsum(underground,
                                      axis=-1,
                                      dtype=float)
 
-        self.initial = np.zeros((self.len_x if self.len_y is
-                                 None else (self.len_x, self.len_y)),
+        # The initial undug state of the mine.
+        self.initial = np.zeros((self.len_x if self.is_2D else
+                                (self.len_x, self.len_y)),
                                 dtype=int)
         self.initial = convert_to_tuple(self.initial)
 
-        # init for BB
-        goal = np.zeros((self.len_x if self.len_y is
-                         None else (self.len_x, self.len_y)),
-                        dtype=int)
-        self.best = convert_to_tuple(goal)
+        # --------------------- init for BB--------------------
+        # Initial best goal state
+        self.best = self.initial
+        # A large starting highest which will be driven down
         self.highest = 1000000000
 
     def surface_neighbours(self, loc):
@@ -249,7 +253,7 @@ class Mine(search.Problem):
             """
             updated = state.copy()
             # Check to see if we have dug to far down.
-            if y is None:  # Its a 1D state we are testing.
+            if self.is_2D:  # Its a 1D state we are testing.
                 np.add.at(updated, x, 1)
                 if updated[x] > self.len_z:
                     return True
@@ -261,7 +265,7 @@ class Mine(search.Problem):
             return self.is_dangerous(updated)
 
         # The generators which will return a valid action.
-        if self.underground.ndim == 2:
+        if self.is_2D:
             return ((x,) for x, z in zip(np.arange(self.len_x), state)
                     if not pass_to(state, x))
         else:
@@ -343,8 +347,9 @@ class Mine(search.Problem):
         '''
         Compute and return the payoff for the given state.
         That is, the sum of the values of all the digged cells.
+        :Param state: a tuple/list of the state to be tested.
 
-        No loops needed in the implementation!
+        :Returns {float}: Of the calculated payoff.
         '''
         # convert to np.array in order to use tuple addressing
         # state[loc]   where loc is a tuple
@@ -352,7 +357,7 @@ class Mine(search.Problem):
         assert not isinstance(state, search.Node)
         state = np.array(state)
 
-        if self.underground.ndim == 2:
+        if self.is_2D:
             # Create a mask using the state and then sum
             mask = state[:, None] > np.arange(self.underground.shape[1])
             return np.sum(self.underground, where=mask)
@@ -364,8 +369,9 @@ class Mine(search.Problem):
     def is_dangerous(self, state):
         '''
         Return True if the given state breaches the dig_tolerance constraints.
+        :Param state: A tuple or list of the given state to be tested.
 
-        No loops needed in the implementation!
+        :Returns {bool}: True if dangerous else False.
         '''
         # convert to np.array in order to use numpy operators
         state = np.array(state)
@@ -376,8 +382,7 @@ class Mine(search.Problem):
         if x_diff.any():
             return True
 
-        # if we have a 3d underground
-        if self.underground.ndim == 3:
+        if not self.is_2D:
             # get the diff along the y dim
             y_diff = np.greater(np.abs(np.diff(state, axis=1)),
                                 self.dig_tolerance)
@@ -395,8 +400,11 @@ class Mine(search.Problem):
 
     def goal_test(self, state):
         """
-        A simple goal test to update the best payoff.
-        :Param state: A state Tuple
+        A simple goal test to update the best payoff for use in the BB
+        function.
+        :Param state: A state Tuple/list or array.
+
+        :Returns {float}: The best states cost value.
         """
         if self.payoff(state) > self.payoff(self.best):
             self.best = state
@@ -408,6 +416,8 @@ class Mine(search.Problem):
         This is the cost function which uses a state and self.cumsum_mine
         to establish a upper for the branches.
         :Param node: either a Node or state tuple.
+
+        :Return {float}: The estimated cost for the node.
         """
         if isinstance(node, search.Node):
             state = np.array(node.state)
@@ -415,7 +425,7 @@ class Mine(search.Problem):
             state = np.array(node)
 
         # Create a mask removing current state values
-        if self.underground.ndim == 2:
+        if self.is_2D:
             mask = state[:, None] > np.arange(self.underground.shape[1])
         else:
             mask = state[:, :, None] > np.arange(self.underground.shape[2])
@@ -447,37 +457,51 @@ def search_dp_dig_plan(mine):
     -------
     best_payoff, best_action_list, best_final_state
     '''
-    def recursive_search(state, act_func, pay_func, rec_func, state_tpl, best_state, actions=None, three_d=False):
+    def recursive_search(state, act_func, pay_func, rec_func, state_tpl,
+                         best_state, actions=None, is_3D=False):
         """
-
+        This is the main recursive function for the DP process.
+        :Param state: the current state as a tuple.
+        :Param act_func: The lru_cache for the action function.
+        :Param pay_func: The lru_cache for the payoff function.
+        :Param rec_func: The lru_cache for the recursive function.
+        :Param state_tpl: A tuple of currently visited states.
+        :Param best_state: The best state as a tuple.
+        :Param actions: A tuple of actions from the state or None.
+        :Param is_3d: If we are checking 2D or 3D mine.
         """
-        #
+        # Change to a list in order to be mutable
         state_lst = convert_to_list(state_tpl)
 
+        # Get the state payoff and check it against the best
         state_payoff = pay_func(state)
 
         if state_payoff > payoff(best_state):
             best_state = state
 
+        # Get a list of actions from this current state.
         given_state_actions = list(act_func(state))
 
+        # Make current state an np array.
         state = np.array(state)
-        if three_d:
+        if is_3D:
             key = str(state.flatten(order="C"))
         else:
             key = str(state)
 
+        # Append the key to the state_lst
         state_lst.append(convert_to_tuple(key))
 
         if len(given_state_actions) > 0:
             for move in given_state_actions:
                 new_state = state.copy()
                 new_state[move] += 1
-                if three_d:
+                if is_3D:
                     key = str(new_state.copy().flatten(order="C"))
                 else:
                     key = str(new_state)
 
+                # If this state currently not in the state_lst we visit it.
                 if convert_to_tuple(key) not in state_lst:
                     best_state = recursive(convert_to_tuple(new_state),
                                            action,
@@ -486,11 +510,11 @@ def search_dp_dig_plan(mine):
                                            convert_to_tuple(state_lst),
                                            best_state,
                                            actions=move,
-                                           three_d=three_d)
+                                           is_3D=is_3D)
         return best_state
 
-    # init return vars.
-    dim_3 = True if mine.underground.ndim == 3 else False
+    # init vars.
+    mine_dim = False if mine.is_2D else True
     best_state = ()
     state = mine.initial
 
@@ -507,7 +531,7 @@ def search_dp_dig_plan(mine):
                            recursive,
                            convert_to_tuple(state_lst),
                            state,
-                           three_d=dim_3)
+                           is_3D=mine_dim)
 
     return mine.payoff(best_state), find_action_sequence(mine.initial, best_state), best_state
 
@@ -612,7 +636,6 @@ def find_action_sequence(s0, s1):
                                 & mask)[0])
 
         if s0[loc] >= s1[loc]:
-            # print(s0, s1)
             mask[loc] = False
             continue
 
